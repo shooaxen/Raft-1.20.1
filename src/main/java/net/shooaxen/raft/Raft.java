28fb80cf16e73a6a26e85d492b4f536d5e82dab9
shooaxen <shooaxen@gmail.com>
2024-08-16 00:50:23 +0800

    added initial files

--- src/main/java/net/shooaxen/raft/Raft.java
+++ src/main/java/net/shooaxen/raft/Raft.java
@@ -7,23 +7,21 @@ import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(RaftStylePack.MOD_ID)
-public class RaftStylePack
+@Mod(Raft.MOD_ID)
+public class Raft
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "raftstylepack";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    public RaftStylePack()
+    public Raft()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
